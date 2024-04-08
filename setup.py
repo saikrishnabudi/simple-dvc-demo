cf0f5fa29f9e175ea23e3572a8fe8cfe06b91dd7
saikrishnabudi <saikrishnabudi@outlook.com>
2024-04-08 13:13:19 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="it is a wine Q package", 
+    author="saikrishnabudi",
+    packages=find_packages(),
+    license="MIT"
+)
